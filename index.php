87e30eb60190637ffde7a1b7d992fb70dd5ecea8
shayanir84 <shayaniranpor8@gmail.com>
2025-09-18 16:35:44 +0330

    compeleted

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
             (isset($_COOKIE["user"]))? $user = $_COOKIE["user"]: $user = '';
             $log = "you are logged in $user";
         }else{
-            header("location:logIn.php");
+            (isset($_COOKIE['user']))? header('Location:logIn.php') :header('Location:signIn.php');
         }
+         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            setcookie("user", "", time() - 0,"/");
+         }
     ?>
 <head>
     <link rel="stylesheet" href="css/style.css">
@@ -16,6 +19,9 @@
     <title>homepage</title>
 </head>
 <body>
+    <form method="post" action="index.php">
+        <button>logOut</button>
+    </form>
     <h1 class="text-center"> mainpage</h1>
     <span class="text-center"><?= $log ?></span>
 </body>
--- signIn.php
+++ signIn.php
@@ -1,37 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
+session_start();
 date_default_timezone_set('Asia/Tehran');
 $server = 'localhost';
 $username_server = 'root';
-$DB = mysqli_connect($server, $username_server, '', 'mydb'); //DB data base
-session_start();
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['user']) && isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['con-pass'])) {
-        $user = htmlspecialchars($_POST['user'], ENT_QUOTES);
-        $email = htmlspecialchars($_POST['email'], ENT_QUOTES);
-        $password = htmlspecialchars($_POST['pass'], ENT_QUOTES);
-        $con_password = htmlspecialchars($_POST['conpass'], ENT_QUOTES);
-        $date = date('Y-m-d-H-i-s');
-        if ($DB) {
-            if (
-                strlen($user) > 2 || strlen($user) < 30
-                && strlen($password) < 8 || strlen($password) > 20
-                && $con_password === $password
-            ) {
-                mysqli_query($DB, "INSERT INTO `user`(`user_name`, `email`, `password`, `created_at`) VALUES ('$user','$email','$password','$date')");
-                setcookie("user", $user, time() + (30 * 24 * 60 * 60), "/");
-                $_SESSION['user'] = $user;
-                header('location:index.php');
-                die('connected');
+$dbpass = '';
+$DB = mysqli_connect($server, $username_server, $dbpass, 'mydb'); //DB data base
+
+if (isset($_COOKIE['user']) && isset($_SESSION['user']) && $_SESSION['user'] == $_COOKIE['user']) {
+    header('location:index.php');
+} else {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (isset($_POST['user']) && isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['con-pass'])) {
+
+            $user = htmlspecialchars($_POST['user'], ENT_QUOTES);
+            $email = htmlspecialchars($_POST['email'], ENT_QUOTES);
+            $password = htmlspecialchars($_POST['pass'], ENT_QUOTES);
+            $con_password = htmlspecialchars($_POST['conpass'], ENT_QUOTES);
+            $date = date('Y-m-d-H-i-s');
+
+            if ($DB) {
+
+                if (
+                    strlen($user) > 2 || strlen($user) < 30
+                    && strlen($password) < 8 || strlen($password) > 20
+                    && $con_password === $password
+                ) {
+                    mysqli_query($DB, "INSERT INTO `user`(`user_name`, `email`, `password`, `created_at`) VALUES ('$user','$email','$password','$date')");
+                    setcookie("user", $user, time() + (30 * 24 * 60 * 60), "/");
+                    $_SESSION['user'] = $user;
+                    header('location:index.php');
+                    die('connected');
+                }
+            } else {
+
+                die("Couldn't Connect");
             }
         } else {
-            die("Couldn't Connect");
+
+            header('location: signIn.php');
         }
     }
 }
-header('location: signIn.php');
-
 ?>
 
 <head>
@@ -43,7 +54,7 @@ header('location: signIn.php');
 
 <body>
     <section class="items-center">
-        <form class="form-control " action="userRigester.php" method="POST">
+        <form class="form-control " action="signIn.php" method="POST">
             <h1 class="text-center">Sign In</h1>
 
             <label class="form-label" for="user-name">User:</label><br>
